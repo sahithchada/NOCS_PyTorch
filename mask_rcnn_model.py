6112a8e14303530f1ca370bb15cf59be20fc6a58
Bharath Sivaram <sivar019@umn.edu>
2023-04-01 10:07:37 -0500

    M mask_rcnn_model.py

--- mask_rcnn_model.py
+++ mask_rcnn_model.py
@@ -8,6 +8,9 @@ import torchvision.transforms as T
 from PIL import Image, ImageEnhance, ImageOps
 import matplotlib.pyplot as plt
 import random
+import sys
+import argparse
+
 
 # Define the function to apply the model to an input image and visualize the results
 def visualize_segmentation(model, image_path, threshold=0.5):
@@ -59,37 +62,47 @@ def iou(mask1, mask2):
     union = (mask1 | mask2).sum()
     return intersection / union
 
-# Load the images from the test_data folder
-test_data_dir = "test_data"
-color_image_paths = sorted([os.path.join(test_data_dir, img) for img in os.listdir(test_data_dir) if img.endswith('_color.png')])
-mask_image_paths = sorted([os.path.join(test_data_dir, img) for img in os.listdir(test_data_dir) if img.endswith('_mask.png')])
 
-model_mask_rcnn= models.detection.maskrcnn_resnet50_fpn(weights="DEFAULT")
-model_mask_rcnn.eval()
+def main():
+    # Load the images from the test_data folder
 
-# Perform the segmentation, visualize the results, and evaluate against ground truth masks
-iou_scores = []
-for color_image_path, mask_image_path in zip(color_image_paths, mask_image_paths):
-    vis_image = visualize_segmentation(model_mask_rcnn, color_image_path)
-    plt.figure()
-    plt.imshow(vis_image)
-    plt.title(os.path.basename(color_image_path))
-    plt.axis('off')
-    
-    # Load the ground truth mask and compute IoU
-    gt_mask = Image.open(mask_image_path).convert('1')
-    gt_mask_array = np.array(gt_mask)
-    pred_mask_array = np.array(vis_image.convert('1'))
-    iou_score = iou(gt_mask_array, pred_mask_array)
-    iou_scores.append(iou_score)
-    print(f"IoU score for {os.path.basename(color_image_path)}: {iou_score:.4f}")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("test_data_dir", help="location of test data with respect to current folder",
+                    type=str)
+    args = parser.parse_args()
 
-# Display all the visualizations
-plt.show()
+    color_image_paths = sorted([os.path.join(args.test_data_dir, img) for img in os.listdir(args.test_data_dir) if img.endswith('_color.png')])
+    mask_image_paths = sorted([os.path.join(args.test_data_dir, img) for img in os.listdir(args.test_data_dir) if img.endswith('_mask.png')])
 
-# Compute and print the average IoU score
-average_iou = sum(iou_scores) / len(iou_scores)
-print(f"Average IoU score: {average_iou:.4f}")
+    model_mask_rcnn= models.detection.maskrcnn_resnet50_fpn(weights="DEFAULT")
+    model_mask_rcnn.eval()
+
+    # Perform the segmentation, visualize the results, and evaluate against ground truth masks
+    iou_scores = []
+    for color_image_path, mask_image_path in zip(color_image_paths, mask_image_paths):
+        vis_image = visualize_segmentation(model_mask_rcnn, color_image_path)
+        plt.figure()
+        plt.imshow(vis_image)
+        plt.title(os.path.basename(color_image_path))
+        plt.axis('off')
+        
+        # Load the ground truth mask and compute IoU
+        gt_mask = Image.open(mask_image_path).convert('1')
+        gt_mask_array = np.array(gt_mask)
+        pred_mask_array = np.array(vis_image.convert('1'))
+        iou_score = iou(gt_mask_array, pred_mask_array)
+        iou_scores.append(iou_score)
+        print(f"IoU score for {os.path.basename(color_image_path)}: {iou_score:.4f}")
+
+        # Display all the visualizations
+        plt.show()
+
+        # Compute and print the average IoU score
+        average_iou = sum(iou_scores) / len(iou_scores)
+        print(f"Average IoU score: {average_iou:.4f}")
+
+if __name__ == "__main__":
+    main()
 
 
 
