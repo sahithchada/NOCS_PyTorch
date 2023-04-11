91b4be5344c9083f9a44ceb6316681a42276a286
sahithchada <117952931+sahithchada@users.noreply.github.com>
2023-04-11 15:17:40 -0500

    model head

--- model.py
+++ model.py
@@ -1024,6 +1024,54 @@ class Mask(nn.Module):
         x = self.sigmoid(x)
 
         return x
+    
+#working 
+# add net_name
+class fpn_coord_bins_graph(nn.Module):
+    def __init__(self,depth,image_shape, pool_size, num_classes, num_bins, net_name):
+        super(fpn_coord_bins_graph, self).__init__()
+        self.pool_size = pool_size
+        self.image_shape = image_shape
+        self.num_classes = num_classes
+        self.num_bins=num_bins
+        self.net_name=net_name
+        self.depth=depth
+
+        self.padding = SamePad2d(kernel_size=3, stride=1)
+        self.conv1 = nn.Conv2d(self.depth, 256, kernel_size=3, stride=1)
+        self.bn1 = nn.BatchNorm2d(256, eps=0.001)
+        self.conv2 = nn.Conv2d(256, 256, kernel_size=3, stride=1)
+        self.bn2 = nn.BatchNorm2d(256, eps=0.001)
+        self.conv3 = nn.Conv2d(256, 256, kernel_size=3, stride=1)
+        self.bn3 = nn.BatchNorm2d(256, eps=0.001)
+        self.conv4 = nn.Conv2d(256, 256, kernel_size=3, stride=1)
+        self.bn4 = nn.BatchNorm2d(256, eps=0.001)
+        self.deconv = nn.ConvTranspose2d(256, 256, kernel_size=2, stride=2)
+        self.conv5 = nn.Conv2d(256, self.num_bins * self.num_classes, kernel_size=1, stride=1)
+        self.sigmoid = nn.Sigmoid()
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, x, rois):
+        x = pyramid_roi_align([rois] + x, self.pool_size, self.image_shape)
+        x = self.conv1(self.padding(x))
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.conv2(self.padding(x))
+        x = self.bn2(x)
+        x = self.relu(x)
+        x = self.conv3(self.padding(x))
+        x = self.bn3(x)
+        x = self.relu(x)
+        x = self.conv4(self.padding(x))
+        x = self.bn4(x)
+        x = self.relu(x)
+        x_feature = self.deconv(x)
+        x = self.relu(x_feature)
+        x = self.conv5(x)
+        x = self.sigmoid(x)
+
+        return x,x_feature
+
 
 
 ############################################################
