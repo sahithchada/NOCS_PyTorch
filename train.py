90affb0cb9018465f88d4ab02f7bbb508e0e3a98
sahithchada <117952931+sahithchada@users.noreply.github.com>
2023-04-14 12:22:57 -0500

    train changes

--- train.py
+++ train.py
@@ -19,11 +19,15 @@ import shutil
 from config import Config
 import utils
 import model as modellib
+from dataset import TrainData
 
 import torch
+import argparse
 
 # Root directory of the project
 ROOT_DIR = os.getcwd()
+
+MODEL_DIR = os.path.join(ROOT_DIR, "models")
 
 # Path to trained weights file
 COCO_MODEL_PATH = os.path.join(ROOT_DIR, "models/mask_rcnn_coco.pth")
@@ -56,6 +60,19 @@ class CocoConfig(Config):
     # Number of classes (including background)
     NUM_CLASSES = 1 + 80  # COCO has 80 classes
 
+class Nocs_train_config(Config):
+    # config file for nocs training, derives from base config  
+    NAME="NOCS train"
+    GPU_COUNT = 0
+    # We use one GPU with 8GB memory, which can fit one image.
+    # Adjust down if you use a smaller GPU.
+    IMAGES_PER_GPU = 16
+
+    # Uncomment to train on 8 GPUs (default is 1)
+    # GPU_COUNT = 8
+
+    # Number of classes (including background)
+    NUM_CLASSES = 1 + 6 # Background plus 6 classes
 
 ############################################################
 #  Dataset
@@ -276,3 +293,71 @@ class CocoDataset(utils.Dataset):
         rle = self.annToRLE(ann, height, width)
         m = maskUtils.decode(rle)
         return m
+
+#main training   
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--gpu',  default='0', type=str)
+    args = parser.parse_args()
+
+    os.environ['CUDA_VISIBLE_DEVICES']=args.gpu
+    print('Using GPU {}.'.format(args.gpu))
+
+    config = Nocs_train_config
+    config.display()
+
+    camera_train_dir = os.path.join('data', 'train')
+
+    #  real classes
+    coco_names = ['BG', 'person', 'bicycle', 'car', 'motorcycle', 'airplane',
+                  'bus', 'train', 'truck', 'boat', 'traffic light',
+                  'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird',
+                  'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear',
+                  'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie',
+                  'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
+                  'kite', 'baseball bat', 'baseball glove', 'skateboard',
+                  'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup',
+                  'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple',
+                  'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
+                  'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed',
+                  'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote',
+                  'keyboard', 'cell phone', 'microwave', 'oven', 'toaster',
+                  'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors',
+                  'teddy bear', 'hair drier', 'toothbrush']
+
+    
+    synset_names = ['BG', #0
+                    'bottle', #1
+                    'bowl', #2
+                    'camera', #3
+                    'can',  #4
+                    'laptop',#5
+                    'mug'#6
+                    ]
+
+
+    class_map = {
+        'bottle': 'bottle',
+        'bowl':'bowl',
+        'cup':'mug',
+        'laptop': 'laptop',
+    }
+
+
+    coco_cls_ids = []
+    for coco_cls in class_map:
+        ind = coco_names.index(coco_cls)
+        coco_cls_ids.append(ind)
+    config.display()
+
+    model = modellib.MaskRCNN(mode="training", config=config,model_dir=MODEL_DIR)
+
+    # initialise woth coco weights
+    model.load_weights(COCO_MODEL_PATH, by_name=True,exclude=["mrcnn_class_logits", "mrcnn_bbox_fc",
+                                                                "mrcnn_bbox", "mrcnn_mask"])
+    
+    dataset_train = TrainData(camera_train_dir,transform = None)
+    
+
+
+
