74958028688d287f00e19e2b1c03dc331e6dd7c4
Bharath <bharathsivaram10@gmail.com>
2023-04-23 16:47:23 -0500

    functions

--- demo.py
+++ demo.py
@@ -15,6 +15,8 @@ import visualize
 import torch
 import cv2
 
+from train import model_loaded_weights
+
 
 # Root directory of the project
 ROOT_DIR = os.getcwd()
@@ -33,6 +35,10 @@ TRAINED_PATH = 'models/nocs_train20230422T1839/mask_rcnn_nocs_train_0025.pth'
 # Directory of images to run detection on
 IMAGE_DIR = os.path.join(ROOT_DIR, "images")
 
+
+# Path to specific image
+IMAGE_SPECIFIC = None
+
 class InferenceConfig(coco.CocoConfig):
     # Set batch size to 1 since we'll be running inference on
     # one image at a time. Batch size = GPU_COUNT * IMAGES_PER_GPU
@@ -43,12 +49,6 @@ class InferenceConfig(coco.CocoConfig):
 
 config = InferenceConfig()
 config.display()
-
-# Create model object.
-model = modellib.MaskRCNN(model_dir=MODEL_DIR, config=config)
-if config.GPU_COUNT:
-    model = model.cuda()
-
 
 #  real classes
 coco_names = ['BG', 'person', 'bicycle', 'car', 'motorcycle', 'airplane',
@@ -91,112 +91,60 @@ for coco_cls in class_map:
     coco_cls_ids.append(ind)
 config.display()
 
-def model_with_weights(mode = 'Trained'):
-
-    model = modellib.MaskRCNN(config=config, model_dir=MODEL_DIR)
-
-    if mode == 'Trained':
-        model.load_state_dict(torch.load(TRAINED_PATH))
-    
-    elif mode == 'COCO':
-
-        pretrained_state_dict = torch.load(COCO_MODEL_PATH)
-
-        filtered_state_dict = pretrained_state_dict
-
-        mismatches = ["classifier.linear_class.weight","classifier.linear_class.bias","classifier.linear_bbox.weight","classifier.linear_bbox.bias","mask.conv5.weight","mask.conv5.bias"]
-
-        for i in range(len(mismatches)):
-
-            weights = filtered_state_dict[mismatches[i]]
-
-            if weights.shape[0] == 81 and weights.dim() > 1:
-                w1 = weights[[0,40,46]]
-                w2 = weights[[64,42]]
-                w3 = torch.zeros_like(w2)
-
-                final_weights = torch.vstack((w1,w3,w2))
-                pass
-
-            # weights shape = (324,1024)
-            # expected 28,1024
-            # 0:3, 160:163, 184:187, 256:259, 168:171
-            elif weights.shape[0] == 324 and len(weights.shape) > 1:
-                weights = torch.reshape(weights, (81,4,1024))
-                # weights = weights.view(weights.size()[0], -1, 4)
-                w1 = weights[[0,40,46]]
-                w2 = weights[[64,42]]
-                w3 = torch.zeros_like(w2)
-
-                final_weights = torch.vstack((w1.flatten(end_dim=-2),w3.flatten(end_dim=-2),w2.flatten(end_dim=-2)))
-
-            elif weights.shape[0] == 324:
-                weights = torch.reshape(weights, (81,4))
-                w1 = weights[[0,40,46]]
-                w2 = weights[[64,42]]
-                w3 = torch.zeros_like(w2)
-
-                final_weights = torch.cat((w1.flatten(),w3.flatten(),w2.flatten()))
-            else:
-                w1 = weights[[0,40,46]]
-                w2 = weights[[64,42]]
-                w3 = torch.zeros_like(w2)
-
-                final_weights = torch.cat((w1,w3,w2))
-
-            filtered_state_dict[mismatches[i]] = final_weights
-
-        model.load_state_dict(filtered_state_dict, strict=False)
-
-    return model
-
-model = model_with_weights()
-
-# Here we get all file names in image dir
-file_names = [f for f in os.listdir(IMAGE_DIR) if f.endswith(( '.png'))]
-
-# Decide between random choice or run on certain image
-# file_name = random.choice(file_names)
-file_name = file_names[8]
-
-print(file_name)
-
-image = skimage.io.imread(os.path.join(IMAGE_DIR, file_name))
-
-if image.shape[2] == 4:
-    image = image[:,:,:3]
-
-# Run detection
-results = model.detect([image])
-
-# Visualize results
-r = results[0]
-
-rois, masks, class_ids, scores, coords = r['rois'], r['masks'], r['class_ids'], r['scores'],r['coords']
-
-visualize.plot_nocs(coords,file_name)
-
-visualize.display_instances(image, rois, masks, class_ids, synset_names,file_name,scores)
-
-plt.figure()
-plt.imshow(masks.sum(2))
-plt.savefig("output_images/mask_out.png")
+model = model_loaded_weights(config,ineference=True,trained_path=TRAINED_PATH)
 
 
-coord_gt = skimage.io.imread('images/0000_coord.png')
-coord_gt_y = coord_gt[:,:,2] 
+def run_model(model,fl_path = None ):
 
-res_y = np.zeros((480,640))
-for i in range(len(coords)):
+    if fl_path:
+        image = skimage.io.imread(fl_path)
+        file_name = fl_path.split('_')[-2].split('/')[-1]
+        print(file_name+'.png')
 
-    cord = coords[i]
+    else:
+        
+        # Here we get all file names in image dir
+        file_names = [f for f in os.listdir(IMAGE_DIR) if f.endswith(( '.png'))]
 
-    y_cord = cord[:,:,2]
-    res_y += y_cord
+        # Decide between random choice or run on certain image
+        file_name = random.choice(file_names)
+        print(file_name)
 
-plt.figure()
-plt.subplot(2,1,1)
-plt.imshow(res_y)
-plt.subplot(2,1,2)
-plt.imshow(coord_gt_y)
-plt.savefig("output_images/y_coord.png")
+        image = skimage.io.imread(os.path.join(IMAGE_DIR, file_name))
+
+    if image.shape[2] == 4:
+        image = image[:,:,:3]  
+
+    # Run detection
+    results = model.detect([image])
+
+    # Visualize results
+    r = results[0]
+
+    rois, masks, class_ids, scores, coords = r['rois'], r['masks'], r['class_ids'], r['scores'],r['coords']
+
+    visualize.plot_nocs(coords,file_name)
+
+    visualize.display_instances(image, rois, masks, class_ids, synset_names,file_name,scores)
+
+
+
+run_model(model,fl_path="data/real/test/0010_color.png")
+
+# coord_gt = skimage.io.imread('images/0000_coord.png')
+# coord_gt_y = coord_gt[:,:,2] 
+
+# res_y = np.zeros((480,640))
+# for i in range(len(coords)):
+
+#     cord = coords[i]
+
+#     y_cord = cord[:,:,2]
+#     res_y += y_cord
+
+# plt.figure()
+# plt.subplot(2,1,1)
+# plt.imshow(res_y)
+# plt.subplot(2,1,2)
+# plt.imshow(coord_gt_y)
+# plt.savefig("output_images/y_coord.png")
