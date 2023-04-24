904b1da81dce5302f21feab86dd05f79e06f7324
Bharath <bharathsivaram10@gmail.com>
2023-04-24 13:10:24 -0500

    now plots the pred and target coords every first step of cal epoch

--- model.py
+++ model.py
@@ -1671,15 +1671,15 @@ class MaskRCNN(nn.Module):
                 mrcnn_coord_z_bin_value = coord_bin_values_module(mrcnn_coord_z_bin).unsqueeze(0)
 
 
-            pred_coords = torch.vstack([mrcnn_coord_x_bin_value,mrcnn_coord_y_bin_value,mrcnn_coord_z_bin_value])
+                pred_coords = torch.vstack([mrcnn_coord_x_bin_value,mrcnn_coord_y_bin_value,mrcnn_coord_z_bin_value])
 
-            # res_pred = torch.empty(3,pred_coords.shape[1],pred_coords.shape[-2],pred_coords.shape[-1])
-            # for i in range(pred_coords.shape[-2]):
-            #     cls = target_class_ids[i]
-            #     temp = pred_coords[:,i,cls]
-            #     res_pred[:,i] = temp
+                res_pred = torch.empty(3,pred_coords.shape[1],pred_coords.shape[-2],pred_coords.shape[-1])
+                for i in range(pred_coords.shape[-2]):
+                    cls = target_class_ids[i]
+                    temp = pred_coords[:,i,cls]
+                    res_pred[:,i] = temp
 
-            # pred_coords = res_pred
+                pred_coords = res_pred
                 
             mrcnn_coords_bin=torch.stack((mrcnn_coord_x_bin,mrcnn_coord_y_bin,mrcnn_coord_z_bin))
 
@@ -1807,25 +1807,6 @@ class MaskRCNN(nn.Module):
             # Run object detection
             rpn_class_logits, rpn_pred_bbox, target_class_ids, mrcnn_class_logits, target_deltas,target_coords,mrcnn_bbox, target_mask, mrcnn_mask, pred_coords,mrcnn_coords_bin = \
                 self.predict([images, image_metas, gt_class_ids, gt_boxes, gt_masks,gt_coords,gt_domain_label], mode='training')
-            
-            # plt.figure()
-            # for i in range(32):
-
-            #     targ_coord = target_coords[:,i].permute(1,2,0)
-            #     plt.subplot(8,4,i+1)
-            #     plt.imshow(targ_coord.detach().cpu().numpy())
-            
-            # plt.savefig('output_images/targ_coords')
-
-            
-            # plt.figure()
-            # for i in range(32):
-
-            #     pred_coord = pred_coords[:,i].permute(1,2,0)
-            #     plt.subplot(8,4,i+1)
-            #     plt.imshow(pred_coord.detach().cpu().numpy())
-            
-            # plt.savefig('output_images/pred_coords')
 
 
             target_domain_labels = torch.tile(gt_domain_label, (1, target_class_ids.shape[0]))
@@ -1890,6 +1871,8 @@ class MaskRCNN(nn.Module):
         loss_y_coord_sum = 0
         loss_z_coord_sum = 0
 
+        plt.figure(1)
+        plt.figure(2)
         for inputs in datagenerator:
             images = inputs[0]
             image_metas = inputs[1]
@@ -1930,6 +1913,27 @@ class MaskRCNN(nn.Module):
                 if self.config.GPU_COUNT:
                     target_domain_labels = target_domain_labels.cuda() 
 
+
+                if step == 0:
+                    plt.figure(1)
+                    for i in range(32):
+
+                        targ_coord = target_coords[:,i].permute(1,2,0)
+                        plt.subplot(8,4,i+1)
+                        plt.imshow(targ_coord.detach().cpu().numpy())
+                    
+                    plt.savefig('output_images/targ_coords')
+
+                    
+                    plt.figure(1)
+                    for i in range(32):
+
+                        pred_coord = pred_coords[:,i].permute(1,2,0)
+                        plt.subplot(8,4,i+1)
+                        plt.imshow(pred_coord.detach().cpu().numpy())
+                    
+                    plt.savefig('output_images/pred_coords')
+
                 # Compute losses
                 rpn_class_loss, rpn_bbox_loss, mrcnn_class_loss, mrcnn_bbox_loss, mrcnn_mask_loss ,coord_bin_loss = compute_losses(rpn_match, rpn_bbox, rpn_class_logits, rpn_pred_bbox, target_class_ids, mrcnn_class_logits, target_deltas, mrcnn_bbox, target_mask, mrcnn_mask, mrcnn_coords_bin,target_coords,target_domain_labels)
                 coord_x_bin_loss = coord_bin_loss[0]
@@ -1946,7 +1950,7 @@ class MaskRCNN(nn.Module):
                                  mrcnn_mask_loss.item(),coord_x_bin_loss.item(),coord_y_bin_loss.item(),coord_z_bin_loss.item()), length=10)
                                 #  mrcnn_class_loss.data.cpu()[0], mrcnn_bbox_loss.data.cpu()[0],
                                 #  mrcnn_mask_loss.data.cpu()[0]), length=10)
-
+            plt.close(1)
             # Statistics
             loss_sum += loss.item()/steps
             loss_rpn_class_sum += rpn_class_loss.item()/steps
